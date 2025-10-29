94d504af7b3229549bc3e5524b20416a67bcf7ad
Ezekiel Prosper <ezekielprosper777@gmail.com>
2025-10-30 00:04:07 +0100

    change to swiper

--- src/components/MemoImagesCell.jsx
+++ src/components/MemoImagesCell.jsx
@@ -7,19 +7,22 @@ import {
   IconButton,
   Typography
 } from "@mui/material";
-import SwipeableViews from "react-swipeable-views";
 import CloseIcon from "@mui/icons-material/Close";
+
+// Swiper imports
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Navigation, Pagination } from "swiper/modules";
+
+// Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
 
 function MemoImagesCell({ memo }) {
   const [open, setOpen] = useState(false);
-  const [index, setIndex] = useState(0); // track current slide
 
-  const handleOpen = () => {
-    setIndex(0); // reset to first image each time
-    setOpen(true);
-  };
+  const handleOpen = () => setOpen(true);
   const handleClose = () => setOpen(false);
-  console.log("Memo images:", memo.images);
 
   return (
     <>
@@ -49,31 +52,35 @@ function MemoImagesCell({ memo }) {
           <CloseIcon />
         </IconButton>
         <DialogContent>
-          <SwipeableViews
-            index={index}
-            onChangeIndex={(i) => setIndex(i)}
-            enableMouseEvents
+          <Swiper
+            modules={[Navigation, Pagination]}
+            navigation
+            pagination={{ clickable: true }}
+            spaceBetween={20}
+            slidesPerView={1}
+            style={{ width: "100%", height: "100%" }}
           >
             {memo.images.map((img, i) => (
-              <div key={i} style={{ textAlign: "center" }}>
-                <img
-                  src={img.image_url}   // 👈 use the property
-                  alt={`Memo Image ${i + 1}`}
-                  style={{
-                    width: "100%",
-                    maxHeight: 500,
-                    objectFit: "contain",
-                    borderRadius: 8
-                  }}
-                />
-              </div>
+              <SwiperSlide key={i}>
+                <div style={{ textAlign: "center" }}>
+                  <img
+                    src={img.image_url}
+                    alt={`Memo Image ${i + 1}`}
+                    style={{
+                      width: "100%",
+                      maxHeight: 500,
+                      objectFit: "contain",
+                      borderRadius: 8
+                    }}
+                  />
+                </div>
+              </SwiperSlide>
             ))}
-
-          </SwipeableViews>
+          </Swiper>
         </DialogContent>
       </Dialog>
     </>
   );
 }
 
-export default MemoImagesCell;
+export default MemoImagesCell;s
